4f4868416ab19c4cb95a568605b5ff69f48c93b9
Jonas Bernst <j.bernst@micromata.de>
2023-10-31 14:18:32 +0100

    init

---
+++ backend/backend/src/main/java/de/micromata/pointcollector/PointCollectorApplication.java
@@ -0,0 +1,13 @@
+package de.micromata.pointcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointCollectorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PointCollectorApplication.class, args);
+  }
+
+}
