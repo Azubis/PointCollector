823b272da49b36371bba217ee9f5f9f55077cd6c
NikitaMic <91180248+NikitaMic@users.noreply.github.com>
2023-12-01 16:12:47 +0100

    temp controller

--- backend/src/main/java/de/micromata/pointcollector/controller_temp/BusinessController.java
+++ backend/src/main/java/de/micromata/pointcollector/controller_temp/BusinessController.java
@@ -1,22 +1,19 @@
-package de.micromata.pointcollector.controller;
+package de.micromata.pointcollector.controller_temp;
 
 import de.micromata.pointcollector.dto.BusinessDTO;
 import de.micromata.pointcollector.models.Business;
 import de.micromata.pointcollector.models.PointUser;
-import de.micromata.pointcollector.models.Product;
 import de.micromata.pointcollector.models.UserPoints;
 import de.micromata.pointcollector.repository.BusinessRepository;
 import de.micromata.pointcollector.repository.PointsRepository;
 import de.micromata.pointcollector.repository.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
